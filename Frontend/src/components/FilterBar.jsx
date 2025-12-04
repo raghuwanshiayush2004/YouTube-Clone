fa6ffaedb154be8a3b1c32e52e869e004dac32b2
DELL <raghuwanshi1405@gmail.com>
2025-12-05 02:47:02 +0530

    handle click

--- Frontend/src/components/FilterBar.jsx
+++ Frontend/src/components/FilterBar.jsx
@@ -22,12 +22,7 @@ const FilterBar = ({ onFilterChange }) => {
   // State to manage the starting index of the filters
   const [startIndex, setStartIndex] = useState(0);
 
-  // Handle previous button click (pagination)
-  const handlePrev = () => {
-    if (startIndex > 0) {
-      setStartIndex(startIndex - itemsPerPage);
-    }
-  };
+ 
 
   // Handle next button click (pagination)
   const handleNext = () => {
